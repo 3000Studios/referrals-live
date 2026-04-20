d41063c2f98930a872b34ee0ff82874cac856f49
3000Studios <mr.jwswain@gmail.com>
2026-04-20 09:03:27 -0400

    Fix JSON helper to preserve headers (Set-Cookie)

--- functions/api/_lib.ts
+++ functions/api/_lib.ts
@@ -8,12 +8,11 @@ export type Env = {
 };
 
 export function json(data: unknown, init: ResponseInit = {}) {
+  const headers = new Headers(init.headers);
+  if (!headers.has("Content-Type")) headers.set("Content-Type", "application/json; charset=utf-8");
   return new Response(JSON.stringify(data), {
     ...init,
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      ...(init.headers ?? {}),
-    },
+    headers,
   });
 }
 
